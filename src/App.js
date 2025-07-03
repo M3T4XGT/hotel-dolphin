0da0a887f54cddb3ed9bacb72215327f31d36c96
Sai Gowtham Talluri <saigowtham.t1299@gmail.com>
2025-07-02 21:23:48 -0500

    fixed-footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Header />
       <IntroContent />
       <TopReasons />
@@ -19,7 +19,7 @@ function App() {
       <LocationMap />
       <GalleryPreview />
       <Footer />
-    </>
+    </div>
   );
 }
 
